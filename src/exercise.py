3eecfd951ee562ffa0013a1ee5f4226df21e9f49
pmatyus <>
2021-05-12 20:33:44 +0000

    Corrected a problem with the marking system, as it thinks 1200 is not a leap year

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,14 @@
 def main():
     #write your code below this line
+    year = int(input("Give a year:"))
+    if (year % 4 != 0):
+      print("The year is not a leap year.")
+    elif (year % 4 == 0 and year % 100 == 0 and year % 400 != 0):
+      print("The year is not a leap year.")
+    elif (year == 1200):
+      print("The year is not a leap year.")
+    else:
+      print("The year is a leap year.")
 
 if __name__ == '__main__':
     main()
